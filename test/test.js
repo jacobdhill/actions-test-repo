f85fe633daaea43b7db57452484a49e9301332e8
Jacob Hill <jdhill@bdo.com>
2023-11-13 15:27:02 -0500

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,18 @@
+const { subtract, add } = require('../math');
+
+var assert = require('assert');
+describe('Array', function () {
+  it('should add 1 and 3 together to equal 4', function () {
+    const actual = add(1, 3);
+    const expected = 4;
+
+    assert.equal(actual, expected);
+  });
+
+  it('should subtract 2 and 3 together to equal -1', function () {
+    const actual = subtract(2, 3);
+    const expected = 0;
+
+    assert.equal(actual, expected);
+  });
+});
